89630b418290d04b53cbad875851cd963a6a07cf
Daniel Adam <danieladamce@gmail.com>
2025-01-12 18:36:33 -0800

    basic serialize interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,6 +2,7 @@ import { Body, ClassSerializerInterceptor, Controller, Delete, Get, NotFoundExce
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
@@ -12,6 +13,7 @@ export class UsersController {
         this.usersService.create(body.email, body.password);
     }
 
+    @UseInterceptors(SerializeInterceptor)
     @Get("/:id")
     async findUser(@Param("id") id: string) {
         const user = await this.usersService.findOne(parseInt(id));
