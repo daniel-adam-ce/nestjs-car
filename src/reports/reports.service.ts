2cad85600ed5b2c8a5e09d7542e242e260d5e59b
Daniel Adam <danieladamce@gmail.com>
2025-01-15 19:22:31 -0800

    getEstimate service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,14 +4,13 @@ import { Repository } from 'typeorm';
 import { Report } from './report.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../users/user.entity';
-import { ApproveReportDto } from './dtos/approve-report.dto';
-import { NotFoundError } from 'rxjs';
+import { GetEstimateDto } from './dtos/get-estimate.dtos';
 
 @Injectable()
 export class ReportsService {
     constructor(
         @InjectRepository(Report) private repo: Repository<Report>
-    ) {  }
+    ) { }
 
     async create(reportDto: CreateReportDto, user: User) {
         const report = this.repo.create(reportDto)
@@ -30,5 +29,20 @@ export class ReportsService {
         report.approved = approved
 
         return this.repo.save(report);
+    }
+
+    async getEstimate({ make, model, lng, lat, year, mileage }: GetEstimateDto) {
+        return this.repo.createQueryBuilder()
+            .select("AVG(price)", "price")
+            .where("make = :make", { make: make })
+            .andWhere("model = :model", { model: model })
+            .andWhere("lng - :lng BETWEEN -5 and 5", { lng })
+            .andWhere("lat - :lat BETWEEN -5 and 5", { lat })
+            .andWhere("year - :year BETWEEN -3 and 3", { year })
+            .andWhere("approved IS TRUE")
+            .orderBy("ABS(mileage - :mileage)", "DESC")
+            .setParameters({ mileage })
+            .limit(3)
+            .getRawOne()
     }
 }
